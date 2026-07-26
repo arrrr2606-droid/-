4c94d7ed2ec3a33c60ac11e85f61e36320d12f7a
Артур Хасанов <alximikufa@gmail.com>
2026-07-26 20:30:12 +0500

    Connect Formspree lead forms

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.ALFA_CONFIG = {"phone": "+7 917 756-26-06", "phoneHref": "+79177562606", "email": "alfa-mash@bk.ru", "formEndpoint": "ВСТАВЬТЕ_СЮДА_URL_FORMSPREE"};
+window.ALFA_CONFIG = {"phone": "+7 917 756-26-06", "phoneHref": "+79177562606", "email": "alfa-mash@bk.ru", "formEndpoint": "https://formspree.io/f/mdaqyojv"};
